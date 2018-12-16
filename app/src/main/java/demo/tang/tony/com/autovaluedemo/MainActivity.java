19f79ad9c4da729070de99880aed8e284fdb1a7d
ztang <tonythompsoncmu@gmail.com>
2018-12-16 12:34:57 -0500

    test out butterknife

--- app/src/main/java/demo/tang/tony/com/autovaluedemo/MainActivity.java
+++ app/src/main/java/demo/tang/tony/com/autovaluedemo/MainActivity.java
@@ -1,13 +1,22 @@
 package demo.tang.tony.com.autovaluedemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_hello)
+    TextView tv_hello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        tv_hello.setText("Butter Knife rocks?\n Yes. It is");
     }
 }
